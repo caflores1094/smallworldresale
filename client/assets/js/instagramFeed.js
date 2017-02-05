a1460fc03ddb7c767b0054e2f2f89cbc1acffd6a
Carlos Flores <caflores1094@gmail.com>
2017-02-05 14:46:59 -0800

    Display instagram Feed on INventory page

--- client/assets/js/instagramFeed.js
+++ client/assets/js/instagramFeed.js
@@ -1,12 +1,18 @@
-// access_token: 277051249.fa47645.cf32f131bc1c4e14a65ada28e6ec2e97
-// client ID: fa476458fa6642bcacb7de11a279ab84
-var feed = new Instafeed({
+//2nd try
+//access token: 277051249.1677ed0.3b8492fbe87c4d8baeb0e51c035890c1
+//My userID: 277051249
+//Small World's ID:
+// userId: '1678137649'
+
+//danielles
+//277051249.1677ed0.3b8492fbe87c4d8baeb0e51c035890c1
+//userID: 276598389
+
+var userFeed = new Instafeed({
   get: 'user',
-  userId: [user_id],
-  accessToken: '[accessToken]',
-  resolution: 'low_resolution',
-  filter: function(image) {
-    return image.tags.indexOf('somehashtag') >= 0;
-  }
+  userId: '277051249',
+  accessToken: '277051249.1677ed0.3b8492fbe87c4d8baeb0e51c035890c1',
+  limit: 40,
+  resolution: 'standard_resolution'
 });
-feed.run();
+userFeed.run();
