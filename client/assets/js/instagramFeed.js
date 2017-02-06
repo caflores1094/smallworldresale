9cfee50e375b4eacb93495ba46fbedca16f08093
Carlos Flores <caflores1094@gmail.com>
2017-02-05 16:28:06 -0800

    Style changes assets and inventory

--- client/assets/js/instagramFeed.js
+++ client/assets/js/instagramFeed.js
@@ -1,18 +1,23 @@
-//2nd try
+//MINE: 
 //access token: 277051249.1677ed0.3b8492fbe87c4d8baeb0e51c035890c1
 //My userID: 277051249
+
 //Small World's ID:
 // userId: '1678137649'
+//client id: 8064044d15684adcb204217687052c6f
+//access token: 1678137649.1677ed0.a10b2f17721f40298228b30c79d4c01d
 
-//danielles
-//277051249.1677ed0.3b8492fbe87c4d8baeb0e51c035890c1
-//userID: 276598389
 
 var userFeed = new Instafeed({
   get: 'user',
-  userId: '277051249',
-  accessToken: '277051249.1677ed0.3b8492fbe87c4d8baeb0e51c035890c1',
-  limit: 40,
+  userId: '1678137649',
+  accessToken: '1678137649.1677ed0.a10b2f17721f40298228b30c79d4c01d',
+  limit: 16,
   resolution: 'standard_resolution'
 });
 userFeed.run();
+
+var loadButton = document.getElementById('load-more');
+loadButton.addEventListener('click', function() {
+  userFeed.next();
+});
