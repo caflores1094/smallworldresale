7c86ec73b3f1d74d57107d68a9bd312758a9d6c7
Carlos Flores <caflores1094@gmail.com>
2017-04-17 21:13:07 -0700

    Edit behavior of emailerRobot

--- server/emailRobot.js
+++ server/emailRobot.js
@@ -18,7 +18,7 @@ var sendEmail = function(trans, emailData){
     // make sure to send it to both the client and the email sender
     let mailOptions = {
         from: emailData.name + ' <' + emailData.email + '>', // sender address
-        to: 'smallworldemailer@gmail.com, ' + emailData.email, // list of receivers
+        to: 'smallworldemailer@gmail.com, ',// + emailData.email, // list of receivers
         subject: 'Interested in Small World Children Resale Boutique', // Subject line
         html: emailMessage // html body
     };
@@ -26,6 +26,27 @@ var sendEmail = function(trans, emailData){
 
     // send mail with defined transport object
     trans.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
+    
+    //Second Email to the Customer
+    //to send two types of emails just do trans.sendMail again with different options
+    var toCustomerIntro = "<p>Thank you for your interest in Small World Children's Resale \
+                            Boutique! Keep an eye out for our response. Below is a copy \
+                            of what you sent us.<br /><br /> \
+                            <i>";
+    var secondMessage = toCustomerIntro + emailMessage + "</i></p>"
+    let mailOptions2 = {
+        from: 'SmallWorldChildrensResale' + ' <smallworldemailer@gmail.com>', // sender address
+        to: emailData.email, // list of receivers
+        subject: 'This is what you sent!', // Subject line
+        html: secondMessage // html body
+    };
+
+    trans.sendMail(mailOptions2, (error, info) => {
         if (error) {
             return console.log(error);
         }
